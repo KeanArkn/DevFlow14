a7f1cbdd7aa4d12c23d0942f9c18bdaee866b2e9
KaanArkan <98814210+KaanArkan@users.noreply.github.com>
2025-01-19 19:31:14 +0300

    implement navbar sidebar localsearch localfilters and homepage

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import RenderTag from './RenderTag'
+
+const RightSidebar = () => {
+    const hotQuestions = [
+        {_id:1, title: 'How do I use express as a custom server in Nextjs?'},
+        {_id:2, title: 'How do I use express as a custom server in Nextjs?'},
+        {_id:3, title: 'How do I use express as a custom server in Nextjs?'},
+        {_id:4, title: 'How do I use express as a custom server in Nextjs?'},
+        {_id:5, title: 'How do I use express as a custom server in Nextjs?'}
+    ]
+    const popularTags = [
+        {_id:1, name:'Javascript', totalQuestions:6},
+        {_id:2, name:'React', totalQuestions:6},
+        {_id:3, name:'Next', totalQuestions:6},
+        {_id:4, name:'Vue', totalQuestions:6},
+        {_id:5, name:'Angular', totalQuestions:6},
+    ]
+  return (
+    <section className='background-light900_dark200 light-border sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden w-[350px] custom-scrollbar' >
+        <div>
+            <h3 className='h3-bold text-dark200_light900'>Top Questions</h3>
+            <div className='mt-7 flex w-full flex-col gap-[30px]'>
+                {hotQuestions.map((question) => (
+                    <Link href={`/questions/${question._id}`} key={question._id} className='flex cursor-pointer items-center justify-between gap-7' >
+                        <p className='body-medium text-dark500_light700'>{question.title}</p>
+                        <Image src='/assets/icons/chevron-right.svg' alt='chevron right' width={20} height={20} className='invert-colors' />
+                    </Link>
+                ))}
+            </div>
+        </div>
+
+        <div className='mt-16 '>
+            <h3 className='h3-bold text-dark200_light900'>Popular Tags</h3>
+            <div className='mt-7 flex flex-col gap-4'>
+                {popularTags.map((tag)=>(
+                    <RenderTag key={tag._id} _id={tag._id} name={tag.name} totalQuestions={tag.totalQuestions} showCount/>
+                ))}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default RightSidebar
